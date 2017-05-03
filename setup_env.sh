9cb8746f81ca022e5bc08f7c9b22b668231d9d99
andela-jmuli <joseph.muli@andela.com>
2017-05-03 19:14:06 +0300

    `add setup script

---
+++ setup_env.sh
@@ -0,0 +1,33 @@
+#! /bin/env bash
+
+javac -version
+if [ $? -ne 0 ]; then
+    echo "No java in your system, Installing Java..."
+    brew cask install java
+fi
+
+echo "installing tools"
+
+declare -a names=(
+    "postgres"
+    "kafka"
+    "redis"
+    "awscli"
+    "docker"
+    "git"
+    "heroku"
+    "sqlite"
+    "node"
+    "postgres"
+    "zookeeper"
+    "kubernetes-cli"
+    "tree"
+    "protobuf"
+    "wget"
+    )
+
+for a in ${names[@]}
+    do
+        brew install ${a}
+    done;
+echo 'Tools upto date!'
